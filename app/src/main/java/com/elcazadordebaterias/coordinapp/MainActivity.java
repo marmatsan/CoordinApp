7148f6d3e33d3fd77f568fa5ff18a5aba95c6bda
Martín Mateos Sánchez <m.mateos97@gmail.com>
2020-11-16 17:46:58 +0100

    Small change

--- app/src/main/java/com/elcazadordebaterias/coordinapp/MainActivity.java
+++ app/src/main/java/com/elcazadordebaterias/coordinapp/MainActivity.java
@@ -7,7 +7,7 @@ import android.os.Bundle;
 public class MainActivity extends AppCompatActivity {
 
     @Override
-    // Función
+    // Funciónd
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
