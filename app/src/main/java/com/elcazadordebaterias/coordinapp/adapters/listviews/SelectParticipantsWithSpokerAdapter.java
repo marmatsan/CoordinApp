b58e37c1ad8c85758a714cbee4526c22d06cb94e
Martín Mateos Sánchez <m.mateos97@gmail.com>
2021-08-25 15:52:48 +0200

    Update

--- app/src/main/java/com/elcazadordebaterias/coordinapp/adapters/listviews/SelectParticipantsWithSpokerAdapter.java
+++ app/src/main/java/com/elcazadordebaterias/coordinapp/adapters/listviews/SelectParticipantsWithSpokerAdapter.java
@@ -1,15 +1,16 @@
 package com.elcazadordebaterias.coordinapp.adapters.listviews;
 
 import android.content.Context;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.CompoundButton;
 import android.widget.RadioButton;
 import android.widget.TextView;
 
 import com.elcazadordebaterias.coordinapp.R;
-import com.elcazadordebaterias.coordinapp.utils.customdatamodels.SelectParticipantItem;
 import com.elcazadordebaterias.coordinapp.utils.customdatamodels.SelectParticipantItemWithSpoker;
 import com.google.android.material.checkbox.MaterialCheckBox;
 
@@ -46,15 +47,26 @@ public class SelectParticipantsWithSpokerAdapter extends BaseAdapter {
         ViewHolder holder;
 
         if (convertView == null) {
-            convertView = LayoutInflater.from(context).inflate(R.layout.utils_selectparticipanwithspokertitem, null);
+            convertView = LayoutInflater.from(context).inflate(R.layout.utils_selectparticipantwithspokertitem, null);
 
             holder = new ViewHolder();
             holder.participantName = convertView.findViewById(R.id.participantName);
             holder.selectParticipant = convertView.findViewById(R.id.selectParticipant);
+            holder.radioButton = convertView.findViewById(R.id.radioButton);
 
             holder.selectParticipant.setOnClickListener(checkbox -> {
                 SelectParticipantItemWithSpoker participant = (SelectParticipantItemWithSpoker) holder.selectParticipant.getTag();
                 participant.setSelected(!participant.isSelected()); // If the participant is selected, unselect it
+                Log.d("DEBUGGING", "Checked");
+            });
+
+            holder.radioButton.setOnCheckedChangeListener((buttonView, isChecked) -> {
+                SelectParticipantItemWithSpoker participant = (SelectParticipantItemWithSpoker) holder.selectParticipant.getTag();
+                for(SelectParticipantItemWithSpoker item : participantsList) {
+                    item.setSpoker(false);
+                    Log.d("DEBUGGING", "Selected spokesman");
+                }
+                participant.setSpoker(true);
             });
 
             convertView.setTag(holder);
@@ -68,6 +80,7 @@ public class SelectParticipantsWithSpokerAdapter extends BaseAdapter {
         holder.participantName.setText(participant.getParticipantName());
         holder.selectParticipant.setTag(participant);
         holder.selectParticipant.setChecked(participant.isSelected());
+        holder.radioButton.setChecked(participant.isSpoker());
 
         return convertView;
     }
--- app/src/main/java/com/elcazadordebaterias/coordinapp/utils/dialogs/teacherdialogs/CreateGroupDialog.java
+++ app/src/main/java/com/elcazadordebaterias/coordinapp/utils/dialogs/teacherdialogs/CreateGroupDialog.java
@@ -14,20 +14,13 @@ import androidx.appcompat.app.AlertDialog;
 import androidx.fragment.app.DialogFragment;
 
 import com.elcazadordebaterias.coordinapp.R;
-import com.elcazadordebaterias.coordinapp.adapters.listviews.SelectParticipantsListAdapter;
 import com.elcazadordebaterias.coordinapp.adapters.listviews.SelectParticipantsWithSpokerAdapter;
 import com.elcazadordebaterias.coordinapp.utils.customdatamodels.SelectParticipantItemWithSpoker;
-import com.elcazadordebaterias.coordinapp.utils.customdatamodels.UserType;
 import com.elcazadordebaterias.coordinapp.utils.firesoredatamodels.Group;
-import com.elcazadordebaterias.coordinapp.utils.firesoredatamodels.CollectiveGroupDocument;
-import com.elcazadordebaterias.coordinapp.utils.firesoredatamodels.PetitionRequest;
-import com.elcazadordebaterias.coordinapp.utils.firesoredatamodels.PetitionUser;
-import com.elcazadordebaterias.coordinapp.utils.customdatamodels.SelectParticipantItem;
 import com.elcazadordebaterias.coordinapp.utils.restmodel.Subject;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.DocumentReference;
-import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FieldPath;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
@@ -73,7 +66,7 @@ public class CreateGroupDialog extends DialogFragment {
         AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
 
         LayoutInflater inflater = getActivity().getLayoutInflater();
-        View view = inflater.inflate(R.layout.utils_creategroupdialog, null);
+        View view = inflater.inflate(R.layout.utils_dialogs_creategroupdialog, null);
 
         // List of participants
         participantsListView = view.findViewById(R.id.participantsList);
@@ -141,17 +134,9 @@ public class CreateGroupDialog extends DialogFragment {
                             .whereIn(FieldPath.documentId(), studentsIDs)
                             .get()
                             .addOnSuccessListener(queryDocumentSnapshots -> {
-
-                                for(int i = 0; i < 200; i++){
-                                    participantsList.add(new SelectParticipantItemWithSpoker("test", "test"));
-
-                                }
-
-                                /*
                                 for (QueryDocumentSnapshot document : queryDocumentSnapshots) {
                                     participantsList.add(new SelectParticipantItemWithSpoker((String) document.get("FullName"), document.getId()));
                                 }
-                                 */
                                 participantsAdapter.notifyDataSetChanged();
                             });
 
